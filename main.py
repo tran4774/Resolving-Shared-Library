88824fa0cc4e28d5f5a5e18b49f830803bdc3893
Tran Nguyen <tran4774@gmail.com>
2022-06-17 05:50:00 +0700

    Fix error no argument given

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os
-import subprocess
-import argparse
+from os import listdir, path, system
+from subprocess import check_output, DEVNULL
+from argparse import ArgumentParser
+from sys import exit
 
 
 def ls_files(directory):
     list_files = list()
-    for item in os.listdir(directory):
-        abspath = os.path.join(directory, item)
+    for item in listdir(directory):
+        abspath = path.join(directory, item)
         try:
-            if os.path.isdir(abspath):
+            if path.isdir(abspath):
                 list_files = list_files + ls_files(abspath)
             else:
                 list_files.append(abspath)
@@ -20,10 +21,10 @@ def ls_files(directory):
 def get_set_shared_object_library_one_file(file):
     set_files = set()
     try:
-        output = subprocess.check_output([f"ldd '{file}' | tr -s '[:blank:]' '\n' | grep '^/'"],
-                                         stderr=subprocess.DEVNULL,
-                                         shell=True,
-                                         encoding='utf8')
+        output = check_output([f"ldd '{file}' | tr -s '[:blank:]' '\n' | grep '^/'"],
+                              stderr=DEVNULL,
+                              shell=True,
+                              encoding='utf8')
         for library_name in output.split("\n"):
             set_files.add(library_name)
     except Exception:
@@ -42,7 +43,7 @@ def get_set_shared_object_library(path):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Adding description")
+    parser = ArgumentParser(description="Adding description")
     parser.add_argument("-p", "--path", help="Path contain all files need to resolve shared object library")
     parser.add_argument("-f", "--file", help="File need to resolve shared object library")
     parser.add_argument("-d", "--destination", help="Destination folder to copy dependencies")
@@ -56,12 +57,15 @@ def main():
         set_dependencies = get_set_shared_object_library(args.path)
     elif args.file:
         set_dependencies = get_set_shared_object_library_one_file(args.file)
+    else:
+        print("No argument --file or --path are given")
+        exit(0)
 
     if len(set_dependencies) == 0:
         print("This file/folder has no dependency")
         exit(0)
     for i in set_dependencies:
-        os.system(f"mkdir -p {des}{i.rpartition('/')[0]}; cp {i} {des}{i}")
+        system(f"mkdir -p {des}{i.rpartition('/')[0]}; cp {i} {des}{i}")
 
 
 main()
